a3bf5700e731dadfdd5967ca2de7e6f7ff8b3eef
Stefan Thalhammer <stefan.thalhammer136@gmail.com>
2018-12-05 17:00:14 +0100

    resolved error

--- RetinaNetPose/bin/train_pose.py
+++ RetinaNetPose/bin/train_pose.py
@@ -343,7 +343,7 @@ def parse_args(args):
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
     parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
 
-    return check_args(parser.parse_args(args))
+    return parser.parse_args(args)
 
 
 def main(args=None):
@@ -360,6 +360,7 @@ def main(args=None):
 
     # optionally choose specific GPU
     if args.gpu:
+        print('specified GPU for training: ', args.gpu)
         os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
     keras.backend.tensorflow_backend.set_session(get_session())
 
